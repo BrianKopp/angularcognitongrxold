3a2a84a357e6722872a9ef04bcf4fd1551f3f8a5
Brian Kopp <briankopp.usa@gmail.com>
2018-09-23 19:17:24 -0700

    Beginning actions

---
+++ src/app/auth/state/auth.action.ts
@@ -0,0 +1,49 @@
+import { Action } from '@ngrx/store';
+import { createActionType } from "./utils";
+
+
+export const LOGIN_USER = createActionType('LOGIN_USER');
+export const LOGIN_USER_SUCCESS = createActionType('LOGIN_USER_SUCCESS');
+export const LOGIN_USER_ERROR = createActionType('LOGIN_USER_ERROR');
+
+export const LOGOUT_USER = createActionType('LOGOUT_USER');
+export const LOGOUT_USER_SUCCESS = createActionType('LOGOUT_USER_SUCCESS');
+export const LOGOUT_USER_ERROR = createActionType('LOGOUT_USER_ERROR');
+
+export class LoginUserAction implements Action {
+    public type: string = LOGIN_USER;
+    constructor(public payload: {username: string, password: string}) {}
+}
+
+export class LoginUserSuccessAction implements Action {
+    public type: string = LOGIN_USER_SUCCESS;
+    constructor(public payload: {User: any}) {}
+}
+
+export class LoginUserErrorAction implements Action {
+    public type: string = LOGIN_USER_ERROR;
+    constructor(public payload?: any) {}
+}
+
+export class LogoutUserAction implements Action {
+    public type: string = LOGOUT_USER;
+    constructor() {}
+}
+
+export class LogoutUserSuccessAction implements Action {
+    public type: string = LOGOUT_USER_SUCCESS;
+    constructor() {}
+}
+
+export class LogoutUserErrorAction implements Action {
+    public type: string = LOGOUT_USER_ERROR;
+    constructor(public payload: string) {}
+}
+
+export type Actions =
+    LoginUserAction
+    | LoginUserSuccessAction
+    | LoginUserErrorAction
+    | LogoutUserAction
+    | LogoutUserSuccessAction
+    | LogoutUserErrorAction;
